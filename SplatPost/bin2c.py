fd1440597c3f81b4efe138c305a7a235bad65f84
bkaufman17 <60152100+bkaufman17@users.noreply.github.com>
2020-05-26 03:51:39 -0400

    Fixed Logic Issues

--- SplatPost/bin2c.py
+++ SplatPost/bin2c.py
@@ -5,11 +5,11 @@ import sys
 data = open(sys.argv[1], 'rb').read()
 
 str_out = "#include <stdint.h>\n\nuint8_t image_data[0x12c1] = {"
-for i in range(0, (320*120) / 8):
+for i in range(0, (320*120) // 8):
    val = 0;
    
    for j in range(0, 8):
-      val |= ord(data[(i * 8) + j]) << j
+      val |= data[(i * 8) + j] << j
    
    val = ~val & 0xFF;
    str_out += hex(val) + ", "
